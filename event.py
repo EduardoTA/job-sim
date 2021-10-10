d461d324f35b1ef74addf92077d5f8cd4e291c05
EduardoTA <EduardoTA@live.com>
2021-10-10 18:54:57 -0300

    Iniciada a classe que representa um evento

---
+++ event.py
@@ -0,0 +1,29 @@
+# Esta classe representa um evento
+# type: tipo do evento de um job
+# tipos:
+# 
+#
+# duration: duração do evento
+# elapsed: tempo decorrido de execução
+
+class Event:
+    type = ""
+    duration = 0
+    elapsed = 0
+
+    # Inicializa o evento
+    def __init__(self, type, duration):
+        self.type = type
+        self.duration = duration
+        self.elapsed = 0
+    
+    # Realiza uma iteração de relógio no evento
+    def iterate(self):
+        self.elapsed -= 1
+    
+    # Verifica se o evento acabou
+    def isOver(self):
+        if self.elapsed >= self.duration:
+            return True
+        else:
+            return False
