301c94fa5e72a612dcef7b059728cfe9fc1ecc33
EduardoTA <EduardoTA@live.com>
2021-10-11 13:25:05 -0300

    Simulador SCA executa os jobs, mas ainda não há a simulação de memória

--- SCA.py
+++ SCA.py
@@ -5,6 +5,7 @@ from job import Job
 t = 0 # Tempo atual de simulação
 flagOver = False # Flag que indica fim de execução
 flagEndEvent = False # Flag que indica que o evento END foi pushed
+flagBusy = False # Flag verdadeira se o processador está ocupado
 
 N = 10000 # Tamanho da memória
 memory = [None]*N # Memória física
@@ -20,12 +21,16 @@ eventsFIFO = []
 
 mallocDuration = 0 # Duração do evento de alocação de memória
 inicDuration = 0 # Duração do evento de inicialização de job
+mfreeDuration = 0 # Duração do evento de liberação de memória
 jentDuration = 0 # Duração de finalizaçao de job
 
 min_job = 0
+
+curEvent = None # Evento que está atualmente sendo executado
+
 # While que itera toda iteração de simulação
 while(flagOver == False):
-
+    # Job handler
     if len(jobs) > 0:
         # Escolhe o job com menor tempo de inicialização
         min_job = jobs[0]
@@ -34,21 +39,48 @@ while(flagOver == False):
                 min_job = job
             if min_job.initTime == t:
                 jobs.remove(min_job)
-                event = Event("INIC", inicDuration, None, min_job)
+                event = Event("INIC", inicDuration, 0, min_job)
                 eventsFIFO.append(event)
                 event = Event("MALLOC", mallocDuration, min_job.mem, min_job)
                 eventsFIFO.append(event)
-                event = Event("EXEC", min_job.duration, None, min_job)
+                event = Event("EXEC", min_job.duration, 0, min_job)
                 eventsFIFO.append(event)
-                event = Event("JEND", jentDuration, None, min_job)
+                event = Event("MFREE", mfreeDuration, 0, min_job)
+                eventsFIFO.append(event)
+                event = Event("JEND", jentDuration, 0, min_job)
                 eventsFIFO.append(event)
     else:
         if flagEndEvent == False:
-            event = Event("END", None, None, None)
+            event = Event("END", 0, 0, 0)
             eventsFIFO.append(event)
             flagEndEvent = True
-            flagOver = True # Para testes
-    t += 1
+    
+    # Event handler
+    
+    if len(eventsFIFO) > 0 and flagBusy == False: # Se a fila de eventos tiver algum evento e o processador não está ocupado
+        curEvent = eventsFIFO.pop(0) # Dequeue de um evento da fila
+        if curEvent.eventType == "END": # Se for evento de finalização
+            print("Evento "+curEvent.__str__()+" terminado em t = "+str(t))
+            flagOver = True # O processador deve finalizar atividades
+        if curEvent.isOver(): # Se o evento que entrou já começar terminado
+            print("Evento "+curEvent.__str__()+" terminado em t = "+str(t))
+        else:
+            flagBusy = True
+    
+    if flagBusy == True:
+        if curEvent.isOver():
+            print("Evento "+curEvent.__str__()+" terminado em t = "+str(t))
+            flagBusy = False
+        else:
+            t += 1
+            curEvent.iterate()
+    if len(eventsFIFO) <= 0:
+        t += 1
+    
+        
+        
+
+    
 
 
 for event in eventsFIFO:
