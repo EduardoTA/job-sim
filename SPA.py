74036ea1985dd852acab5dc47cf191738969bc8a
EduardoTA <EduardoTA@live.com>
2021-10-16 19:24:31 -0300

    Job só entra na fila de execução se todos os jobs estiverem em execução

--- SPA.py
+++ SPA.py
@@ -1,6 +1,35 @@
 from event import Event
 from job import Job
 
+def checkAllJobsExec(jobs):
+    flag = True
+    for job in jobs:
+        if job.events[0].eventType != "EXEC":
+            flag = False
+    return flag
+
+def firstFit(size, memory): # Retorna o índice do endereço de memória onde começa com espaço vazio com tamanho pelo menos igual a size
+    emptyLen = 0 # Tamanho do espaço vazio
+    emptyStart = 0 # Começo do espaço vazio
+    i = 0 # Índice que vai marchando pelo vetor de memória
+    flagFoundEmpty = False # Flag que indica que espaço vazio foi encontrado
+    while emptyLen < size and i < len(memory):
+        if memory[i] == None:
+            if flagFoundEmpty == False:
+                flagFoundEmpty = True
+                emptyStart = i
+                emptyLen = 1
+            else:
+                emptyLen += 1
+        else:
+            emptyLen = 0
+            flagFoundEmpty = False
+        i += 1
+    if emptyLen >= size:
+        return emptyStart
+    else:
+        return -1
+
 # Implementação da Alocação Particionada Simples
 t = 0 # Tempo atual de simulação
 flagOver = False # Flag que indica fim de execução
@@ -10,9 +39,17 @@ flagBusy = False # Flag verdadeira se o processador está ocupado
 N = 100 # Tamanho da memória
 memory = [None]*N # Memória física
 
+mallocDuration = 0 # Duração do evento de alocação de memória
+inicDuration = 0 # Duração do evento de inicialização de job
+mfreeDuration = 0 # Duração do evento de liberação de memória
+jentDuration = 0 # Duração de finalizaçao de job
+
 job1 = Job("Job1", 77, 397, 90) # Job 1
+job1.spawnEvents(inicDuration, mallocDuration, mfreeDuration, jentDuration) # Cria os eventos do job
 job2 = Job("Job2", 373, 73, 5) # Job 2
+job2.spawnEvents(inicDuration, mallocDuration, mfreeDuration, jentDuration) # Cria os eventos do job
 job3 = Job("Job3", 77,  88, 20) # Job 2
+job3.spawnEvents(inicDuration, mallocDuration, mfreeDuration, jentDuration) # Cria os eventos do job
 
 jobsFIFO = [] # Fila de jobs que não estão sendo executados
 jobsFIFO.append(job1)
@@ -25,73 +62,74 @@ jobsFIFO.sort(key=lambda x: x.initTime)
 # Fila de eventos
 eventsFIFO = []
 
-mallocDuration = 0 # Duração do evento de alocação de memória
-inicDuration = 0 # Duração do evento de inicialização de job
-mfreeDuration = 0 # Duração do evento de liberação de memória
-jentDuration = 0 # Duração de finalizaçao de job
-
 curEvent = None # Evento que está atualmente sendo executado
 
 multi = 2 # Grau máximo de multiprogramação
 
-jobFetchFIFO = [] # Fila com os jobs na fila para execução
+jobFetchFIFO = [] # Fila com os jobs cujo tempo de início passou
 
-jobExecFIFO = [] # Fila com os jobs sendo executados no momento
+jobExecFIFO = [] # Fila com os jobs alocados no momento (sendo executados)
 jobExecFIFOIndex = 0 # Elemento da fila que está sendo executado no momento
 
 turnoverTime = 20 # Número de ciclos de clock para trocar de job
 
 # While que itera toda iteração de simulação
-while(flagOver == False and t<100021):
+while(flagOver == False and t<10001):
     # Job fetcher
     if len(jobsFIFO) > 0:
         while(len(jobsFIFO) > 0 and jobsFIFO[0].initTime == t):
             jobFetchFIFO.append(jobsFIFO[0])
             jobsFIFO.pop(0)
 
+    #emptyStart = 0 # Posição de início de espaço vazio
     # Job issuer
     if len(jobFetchFIFO) > 0 and len(jobExecFIFO) < multi:
-        jobExecFIFO.append(jobFetchFIFO[0])
-        jobFetchFIFO.pop(0)
-    
-    # Job scheduler
-    if len(jobExecFIFO) > 0 and (flagBusy == False or t % multi == 0):
-        if len(jobExecFIFO) == 1:
-            flagBusy == True
-
-            eventsFIFO = [] # Limpar fila de eventos
-            event = Event("INIC", inicDuration, 0, jobExecFIFO[0])
-            eventsFIFO.append(event)
-            event = Event("MALLOC", mallocDuration, jobExecFIFO[0].mem, jobExecFIFO[0])
-            eventsFIFO.append(event)
-            event = Event("EXEC", jobExecFIFO[0].duration, 0, jobExecFIFO[0])
-            eventsFIFO.append(event)
-            event = Event("MFREE", mfreeDuration, 0, jobExecFIFO[0])
-            eventsFIFO.append(event)
-            event = Event("JEND", jentDuration, 0, jobExecFIFO[0])
-            eventsFIFO.append(event)
+        if len(jobExecFIFO) == 0:
+            jobExecFIFO.append(jobFetchFIFO[0])
+            jobFetchFIFO.pop(0)
         else:
-            flagBusy == True
-            # Index do job a ser executado no próximo turnover
-            if jobExecFIFOIndex == len(jobExecFIFO)-1:
-                jobExecFIFOIndex = 0
-            else:
-                jobExecFIFOIndex += 1
+            if checkAllJobsExec(jobExecFIFO):
+                emptyStart = firstFit(jobFetchFIFO[0].mem, memory)
+                print(emptyStart)
+                if emptyStart > -1:
+                    jobExecFIFO.append(jobFetchFIFO[0])
+                    jobFetchFIFO.pop(0)
+                else:
+                    pass
 
-            eventsFIFO = [] # Limpar fila de eventos
-            event = Event("INIC", inicDuration, 0, jobExecFIFO[jobExecFIFOIndex])
-            eventsFIFO.append(event)
-            event = Event("MALLOC", mallocDuration, jobExecFIFO[jobExecFIFOIndex].mem, jobExecFIFO[jobExecFIFOIndex])
-            eventsFIFO.append(event)
-            event = Event("EXEC", jobExecFIFO[jobExecFIFOIndex].duration, 0, jobExecFIFO[jobExecFIFOIndex])
-            eventsFIFO.append(event)
-            event = Event("MFREE", mfreeDuration, 0, jobExecFIFO[jobExecFIFOIndex])
-            eventsFIFO.append(event)
-            event = Event("JEND", jentDuration, 0, jobExecFIFO[jobExecFIFOIndex])
-            eventsFIFO.append(event)
+    # Job scheduler
+    # if len(jobExecFIFO) > 0 and (flagBusy == False or t % multi == 0):
+    #     #if len(eventsFIFO) 
+    #     if len(jobExecFIFO) == 1:
+    #         flagBusy == True
+
+    #         eventsFIFO = [] # Limpar fila de eventos
+    #         jobExecFIFO[0].spawnEvents(inicDuration, mallocDuration, mfreeDuration, jentDuration)
+    #         eventsFIFO += jobExecFIFO[0].events
+    #     else:
+    #         flagBusy == True
+    #         eventsFIFO = [] # Limpar fila de eventos
+    #         jobExecFIFO[jobExecFIFOIndex].spawnEvents(inicDuration, mallocDuration, mfreeDuration, jentDuration)
+    #         eventsFIFO += jobExecFIFO[jobExecFIFOIndex].events
+
+    #         # Index do job a ser executado no próximo turnover
+    #         if jobExecFIFOIndex == len(jobExecFIFO)-1:
+    #             jobExecFIFOIndex = 0
+    #         else:
+    #             jobExecFIFOIndex += 1
+
+            
 
 
     t += 1
+
+print('\njobsFIFO[]:')
+for job in jobsFIFO:   
+    print(job) 
+
+print('\njobFetchFIFO[]:')
+for job in jobFetchFIFO:   
+    print(job) 
 
 print('\njobExecFIFO[]:')
 for job in jobExecFIFO:   
--- job.py
+++ job.py
@@ -1,9 +1,11 @@
+from event import Event
+
 class Job:
     initTime = 0 # Tempo de início do job
     name = "" # Nome do job
     duration = 0 # Duração do job
     mem = 0 # Memória alocada para o job
-    state = "INIC"
+    events = [] # Eventos pendentes relacionados ao job
 
     # Inicializa o evento
     def __init__(self, name, initTime, duration, mem):
@@ -11,6 +13,20 @@ class Job:
         self.mem = mem
         self.initTime = initTime
         self.name = name
+
+    # Cria os eventos iniciais
+    def spawnEvents(self, inicDuration, mallocDuration, mfreeDuration, jentDuration):
+        self.events = []
+        event = Event("INIC", inicDuration, 0, self)
+        self.events.append(event)
+        event = Event("MALLOC", mallocDuration, self.mem, self)
+        self.events.append(event)
+        event = Event("EXEC", self.duration, 0, self)
+        self.events.append(event)
+        event = Event("MFREE", mfreeDuration, 0, self)
+        self.events.append(event)
+        event = Event("JEND", jentDuration, 0, self)
+        self.events.append(event)
 
     def __str__(self):
         return str(self.name)
